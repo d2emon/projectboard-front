42e75df44c9ff5bf32ee08be2440544390127c70
Dmitry Kutsenko <d2emonium@gmail.com>
2018-01-26 16:00:59 +0300

    Cards

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,150 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
+    <div transition="fade-transition">
+      <v-layout row wrap>
+        <v-flex sm6 lg3>
+          <v-card dark color="primary">
+            <v-card-text class="width: 100%;">
+              <v-btn icon slot="activator" absolute right>
+                <v-icon>settings</v-icon>
+              </v-btn>
+              <v-menu>
+                <v-btn icon slot="activator">
+                  <v-icon>settings</v-icon>
+                </v-btn>
+                <v-list>
+                  <v-list-tile>
+                    <v-list-tile-title>Action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Another action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Something else here...</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title disabled>Disabled action</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+              <h4 class="mb-0">9.823</h4>
+              <p>Members online</p>
+              <div style="height: 200px;">Chart</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex sm6 lg3>
+          <v-card dark color="primary">
+            <v-card-text class="width: 100%;">
+              <v-btn icon slot="activator" absolute right>
+                <v-icon>settings</v-icon>
+              </v-btn>
+              <v-menu>
+                <v-btn icon slot="activator">
+                  <v-icon>settings</v-icon>
+                </v-btn>
+                <v-list>
+                  <v-list-tile>
+                    <v-list-tile-title>Action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Another action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Something else here...</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title disabled>Disabled action</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+              <h4 class="mb-0">9.823</h4>
+              <p>Members online</p>
+              <div style="height: 200px;">Chart</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex sm6 lg3>
+          <v-card dark color="primary">
+            <v-card-text class="width: 100%;">
+              <v-btn icon slot="activator" absolute right>
+                <v-icon>settings</v-icon>
+              </v-btn>
+              <v-menu>
+                <v-btn icon slot="activator">
+                  <v-icon>settings</v-icon>
+                </v-btn>
+                <v-list>
+                  <v-list-tile>
+                    <v-list-tile-title>Action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Another action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Something else here...</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title disabled>Disabled action</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+              <h4 class="mb-0">9.823</h4>
+              <p>Members online</p>
+              <div style="height: 200px;">Chart</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex sm6 lg3>
+          <v-card dark color="primary">
+            <v-card-text class="width: 100%;">
+              <v-btn icon slot="activator" absolute right>
+                <v-icon>settings</v-icon>
+              </v-btn>
+              <v-menu>
+                <v-btn icon slot="activator">
+                  <v-icon>settings</v-icon>
+                </v-btn>
+                <v-list>
+                  <v-list-tile>
+                    <v-list-tile-title>Action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Another action</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title>Something else here...</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-title disabled>Disabled action</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+              <h4 class="mb-0">9.823</h4>
+              <p>Members online</p>
+              <div style="height: 200px;">Chart</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex sm6 lg3>
+           <v-card dark color="secondary" class="width: 100%;">
+             <v-card-text class="px-0">
+               Chart
+             </v-card-text>
+           </v-card>
+        </v-flex>
+        <v-flex sm6 lg3>
+           <v-card dark color="secondary">
+             <v-card-text class="px-0">6</v-card-text>
+           </v-card>
+        </v-flex>
+        <v-flex sm6 lg3>
+           <v-card dark color="secondary">
+             <v-card-text class="px-0">6</v-card-text>
+           </v-card>
+        </v-flex>
+      </v-layout>
+
       <v-layout column align-center>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
@@ -12,7 +156,7 @@
           </footer>
         </blockquote>
       </v-layout>
-    </v-slide-y-transition>
+    </div>
   </v-container>
 </template>
 
@@ -20,14 +164,6 @@
 <style scoped>
 h1, h2 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
