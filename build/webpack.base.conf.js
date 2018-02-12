fb2290fec1e49e3f7acc0305c8721c0e88416aa1
Dmitry Kutsenko <d2emonium@gmail.com>
2018-02-12 16:05:59 +0300

    BundleTracker

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var BundleTracker = require('webpack-bundle-tracker')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -25,6 +26,11 @@ module.exports = {
       '@': resolve('src')
     }
   },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
   module: {
     rules: [
       {
